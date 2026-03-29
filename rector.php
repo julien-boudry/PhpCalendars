077abaa861dcee7da02c7eb7f03ee9c691111fbc
Julien Boudry <julien.boudry@proton.me>
2026-03-29 15:36:02 +0200

    add rector.php configuration

---
+++ rector.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\{RemoveUselessParamTagRector, RemoveUselessReturnTagRector};
+use Rector\Instanceof_\Rector\Ternary\FlipNegatedTernaryInstanceofRector;
+use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+use Rector\TypeDeclaration\Rector\While_\WhileNullableToInstanceofRector;
+
+$rectorLevel = 17;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/Benchmarks',
+        __DIR__ . '/Dev',
+        __DIR__ . '/Examples',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    ->withIndent(indentChar: ' ', indentSize: 4)
+    ->withPhpSets()
+    ->withTypeCoverageLevel($rectorLevel)
+    ->withDeadCodeLevel($rectorLevel)
+    ->withCodeQualityLevel($rectorLevel)
+
+    ->withRules([
+        FlipNegatedTernaryInstanceofRector::class,
+        WhileNullableToInstanceofRector::class,
+        ClassPropertyAssignToConstructorPromotionRector::class,
+        RemoveUselessReturnTagRector::class,
+        RemoveUselessParamTagRector::class,
+    ])
+
+    ->withSkip([
+        ClassPropertyAssignToConstructorPromotionRector::class,
+        NullToStrictStringFuncCallArgRector::class,
+        RestoreDefaultNullToNullableTypePropertyRector::class,
+    ])
+;
